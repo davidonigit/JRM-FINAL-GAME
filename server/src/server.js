ce79b5d3079042701f8503d9bc4cae458035d388
Davi Duarte <davi.duarte11@gmail.com>
2023-06-27 12:02:35 -0300

    lobby and rooms, game start point

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-var express = require ('express')
+var express = require('express')
 var http = require('http')
 var Server = require('socket.io').Server
 var cors = require('cors');
@@ -8,104 +8,112 @@ var app = express();
 app.use(cors())
 
 app.get('/', (request, response) => {
-  response.send('RAIZ do servidor web')
+	response.send('RAIZ do servidor web')
 });
 
 var httpServer = http.createServer(app);
 var io = new Server(httpServer, {
-  cors: {
-    origin: "*"
-  }
+	cors: {
+		origin: "*"
+	}
 });
 let cadastros = [];
 let players = [];
+let salas = [];
+let jogos = {};
 
 function findBySocketId(socketId) {
-  return players.find((e) =>  e.socketId === socketId)
+	return players.find((e) => e.socketId === socketId)
 }
 
 function addPlayer(nome, socketId) {
-    const player = {
-      nome: nome,
-      socketId: socketId
-    }
-    players.push(player);
-    io.emit('players', players);
+	const player = {
+		nome: nome,
+		socketId: socketId
+	}
+	players.push(player);
+	io.emit('players', players);
 }
 
 io.on('connection', (clientSocket) => {
-  console.log('usuario conectado')
-  console.log(clientSocket.id)
+	console.log('usuario conectado')
+	//console.log(clientSocket.id)
 
-  
-  clientSocket.on('disconnect', () => {
-    let index = players.indexOf(clientSocket.id)
-    players.splice(index,1)
-    console.log('usuario desconectado')
-  })
-  
 
-  clientSocket.emit('players', players);
-  
-  clientSocket.on('criarConta', (data) => {
-    const existingAccount = cadastros.filter((e) => e.username === data.username || e.email === data.email)
+	clientSocket.on('disconnect', () => {
+		let index = players.map(e => e.socketId).indexOf(clientSocket.id)
+		if (index != -1) {
+			players.splice(index, 1)
+		}
+		console.log('usuario desconectado')
+	})
 
-    if (existingAccount.length > 0) {
-      // já existe
-      clientSocket.emit('cadastroFalhou')
-    } else{
-      const newAccount = {
-        username: data.username,
-        email: data.email,
-        senha: data.senha
-      }
-      cadastros.push(newAccount)
-      console.log(cadastros)
-      clientSocket.emit('cadastroSucesso')
-    }
-  })
-  
-  clientSocket.on('tentarLogin', (data) => {
-    const loginValido = cadastros.filter((e) => e.username === data.login && e.senha === data.senha)
+	clientSocket.emit('salas', salas)
+	clientSocket.emit('players', players);
 
-    if (loginValido.length > 0) {
-      clientSocket.emit('loginSucesso')
-    } else{
-      clientSocket.emit('loginFalhou')
-    }
-  })
+	clientSocket.on('criarConta', (data) => {
+		const existingAccount = cadastros.filter((e) => e.username === data.username || e.email === data.email)
 
-  clientSocket.on('entrar', (data) => {
-    const contaOnline = players.filter((e) => e.nome === data.nome)
+		if (existingAccount.length > 0) {
+			// já existe
+			clientSocket.emit('cadastroFalhou')
+		} else {
+			const newAccount = {
+				username: data.username,
+				email: data.email,
+				senha: data.senha
+			}
+			cadastros.push(newAccount)
+			console.log(cadastros)
+			clientSocket.emit('cadastroSucesso')
+		}
+	})
 
-    if (contaOnline > 0) {
-      contaOnline.socketId = clientSocket.id
-    } else{
-      addPlayer(data.nome, clientSocket.id);
-      console.log(data.nome + ' entrou no jogo');
-    }
-    
-    // mandar players para todos
-    //io.emit('players', players);
-  });
+	clientSocket.on('tentarLogin', (data) => {
+		const loginValido = cadastros.filter((e) => e.username === data.login && e.senha === data.senha)
 
-  clientSocket.on('enviarMensagem', (data) => {
-    const remetente = findBySocketId(clientSocket.id);
-    const msg = `<strong>${remetente.nome}</strong>: ${data.texto}`;
-    io.emit('postarMensagem', msg)
-  })
-}
-)
+		if (loginValido.length > 0) {
+			clientSocket.emit('loginSucesso')
+		} else {
+			clientSocket.emit('loginFalhou')
+		}
+	})
+
+	clientSocket.on('entrar', (data) => {
+
+		addPlayer(data.nome, clientSocket.id);
+		console.log(data.nome + ' entrou no jogo');
+
+		// mandar players para todos
+		//io.emit('players', players);
+	});
+
+	clientSocket.on('enviarMensagem', (data) => {
+		const remetente = findBySocketId(clientSocket.id);
+		const msg = `<strong>${remetente.nome}</strong>: ${data.texto}`;
+		io.emit('postarMensagem', msg)
+	})
+
+	clientSocket.on('criarSala', sala => {
+		const salaExistente = salas.filter((e) => e === sala)
+
+		if(salaExistente.length === 0){
+			salas.push(sala)
+		}
+		io.emit('salas', salas)
+		console.log(salas)
+	})
+})
 
 // ficar mandando a lista de player para todo mundo a cada 1 segundo
 setInterval(() => {
-  io.emit('players', players)
-  console.log(players)
+	io.emit('players', players)
+	io.emit('salas', salas)
 }, 10000)
 
 
 
 httpServer.listen(3000, () => {
-  console.log('servidor iniciou na porta 3000')
+	console.log('servidor iniciou na porta 3000')
 }
 )
